d93fb670f4ee9f31961132b8e4db59da2e98e74f
mrevanzak <mrevanzak@gmail.com>
2022-06-29 10:09:30 +0700

    update navbar for getting into rewards page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,65 +1,72 @@
-import Link from "next/link";
-import Container from "components/Container";
-import Image from "next/image";
-import logo from "../assets/logo.png";
-import { useContext } from "react";
-import { UserContext } from "contexts";
-import { UserContextType } from "@customTypes/type";
-import { useRouter } from "next/router";
-import { CurrencyEuroIcon } from "@heroicons/react/outline";
+import Link from "next/link"
+import Container from "components/Container"
+import Image from "next/image"
+import logo from "../assets/logo.png"
+import { useContext } from "react"
+import { UserContext } from "contexts"
+import { UserContextType } from "@customTypes/type"
+import { useRouter } from "next/router"
+import { CurrencyEuroIcon } from "@heroicons/react/outline"
 
 const Navbar = () => {
-    const { user } = useContext(UserContext) as UserContextType;
-    const router = useRouter();
+    const { user } = useContext(UserContext) as UserContextType
+    const router = useRouter()
 
     return (
-        <nav className="py-10">
+        <nav className='py-10'>
             <Container>
-                <div className="flex justify-between items-center text-gray-900">
-                    <div className="lg:w-2/12 w-6/12">
-                        <Link href="/">
-                            <a className="flex items-center justify-center lg:justify-start">
+                <div className='flex justify-between items-center text-gray-900'>
+                    <div className='lg:w-2/12 w-6/12'>
+                        <Link href='/'>
+                            <a className='flex items-center justify-center lg:justify-start'>
                                 <Image src={logo} />
                             </a>
                         </Link>
                     </div>
-                    <div className="flex flex-col items-end">
+                    <div className='flex flex-col items-end'>
                         {user.name ? (
-                            <>
-                                <Link href="/user">
-                                    <a className="flex-shrink-0 group block">
-                                        <div className="flex items-center">
-                                            <div className="mr-3 flex flex-col items-end">
-                                                <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
-                                                    {user.name}
-                                                </p>
-                                                <div className="flex items-center text-xs font-medium text-gray-500 group-hover:text-gray-700">
-                                                    <CurrencyEuroIcon className="h-5 w-5 inline pr-0.5" />
-                                                    <p>
-                                                        {user.balance.toLocaleString(
-                                                            "id-ID",
-                                                        )}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                            <div className="flex items-center">
+                            <div className='flex-shrink-0 group block'>
+                                <div className='flex items-center'>
+                                    <div className='mr-3 flex flex-col items-end'>
+                                        <Link href='/user'>
+                                            <a className='text-sm font-medium text-gray-700 group-hover:text-gray-900'>
+                                                {user.name}
+                                            </a>
+                                        </Link>
+
+                                        <div className='flex items-center text-xs font-medium text-gray-500 group-hover:text-gray-700'>
+                                            <CurrencyEuroIcon className='h-5 w-5 inline pr-0.5' />
+                                            <p>{user.balance.toLocaleString("id-ID")}</p>
+                                        </div>
+                                        {Math.floor(user.totalSpent / 50000) > 0 && (
+                                            <Link href='/rewards'>
+                                                <a className='text-xs font-medium text-sky-500 group-hover:text-gray-900'>
+                                                    You have {Math.floor(user.totalSpent / 50000)} chance for lucky draw
+                                                </a>
+                                            </Link>
+                                        )}
+                                    </div>
+                                    <div className='flex items-center'>
+                                        <Link href='/user'>
+                                            <a>
                                                 <Image
-                                                    className="inline-block h-9 w-9 rounded-full"
-                                                    src="https://avatars.dicebear.com/api/adventurer-neutral/izone.svg"
-                                                    alt=""
+                                                    className='inline-block h-9 w-9 rounded-full'
+                                                    src='https://avatars.dicebear.com/api/adventurer-neutral/izone.svg'
+                                                    alt=''
                                                     height={36}
                                                     width={36}
                                                 />
-                                            </div>
-                                        </div>
-                                    </a>
-                                </Link>
-                            </>
+                                            </a>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
                         ) : (
                             <button
-                                type="button"
-                                className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-sky-500 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
-                                onClick={() => router.reload()}>
+                                type='button'
+                                className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-sky-500 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500'
+                                onClick={() => router.reload()}
+                            >
                                 Login
                             </button>
                         )}
@@ -67,7 +74,7 @@ const Navbar = () => {
                 </div>
             </Container>
         </nav>
-    );
-};
+    )
+}
 
-export default Navbar;
+export default Navbar
